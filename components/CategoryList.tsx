13fbc3399652e314e3ec5a65f19bc83ab5ae201c
manhkien198 <manhkien1998@gmail.com>
2022-05-10 16:56:22 +0700

    update

--- components/CategoryList.tsx
+++ components/CategoryList.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Image, View, Text } from "react-native";
 const CategoryList = () => {
-  return <View></View>;
+  return (
+    <View>
+      <Text>Hello native</Text>
+    </View>
+  );
 };
 export default CategoryList;
