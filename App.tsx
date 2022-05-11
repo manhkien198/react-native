9d1d81a67e38e4eb4e5df349c719eaa0e8783e98
manhkien198 <manhkien1998@gmail.com>
2022-05-11 16:52:54 +0700

    update

--- App.tsx
+++ App.tsx
@@ -1,34 +1,20 @@
-import { FlatList, ScrollView, StyleSheet, View } from "react-native";
-import Diet from "./assets/diet.png";
-import Clothes from "./assets/fashion.png";
-import Meds from "./assets/meds.png";
-import Shampoo from "./assets/shampoo.png";
-import Soap from "./assets/soap.png";
-import CategoryList from "./components/CategoryList";
-interface Category {
-  id: number;
-  title: string;
-  image: HTMLImageElement;
-}
+import { StyleSheet } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Category from "./screens/Category";
+import Categories from "./screens/Categories";
+import { NavigationContainer } from "@react-navigation/native";
+import "react-native-gesture-handler";
+import * as React from "react";
 export default function App() {
-  const category: Array<Category> = [
-    { id: 1, title: "Thực phẩm chức năng", image: Meds },
-    { id: 2, title: "Sữa tắm", image: Soap },
-    { id: 3, title: "Dầu gội", image: Shampoo },
-    { id: 4, title: "Thực phẩm", image: Diet },
-    { id: 5, title: "Quần áo", image: Clothes },
-  ];
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={category}
-        renderItem={({ item }) => (
-          <CategoryList title={item.title} image={item.image} />
-        )}
-        keyExtractor={(item) => `${item.id}`}
-        contentContainerStyle={{ paddingLeft: 16, paddingRight: 16 }}
-      />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Category" component={Category} />
+        <Stack.Screen name="Categories" component={Categories} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
