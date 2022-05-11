5065741bee0e3ec28e611ef7eae290a23623fda9
manhkien198 <manhkien1998@gmail.com>
2022-05-11 13:51:51 +0700

    commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import CategoryList from "./components/CategoryList";
-
+import Meds from "./assets/meds.png";
+import Soap from "./assets/soap.png";
+import Shampoo from "./assets/shampoo.png";
 export default function App() {
   return (
     <View style={styles.container}>
-      <CategoryList />
-      <StatusBar style="auto" />
+      <CategoryList title="Thực phẩm chức năng" image={Meds} />
+      <CategoryList title="Sữa tắm" image={Soap} />
+      <CategoryList title="Dầu gội" image={Shampoo} />
     </View>
   );
 }
@@ -15,7 +18,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+    alignItems: "stretch",
     justifyContent: "center",
+    paddingLeft: 16,
+    paddingRight: 16,
   },
 });
--- components/CategoryList.tsx
+++ components/CategoryList.tsx
@@ -1,10 +1,53 @@
 import React from "react";
-import { Image, View, Text } from "react-native";
-const CategoryList = () => {
+import {
+  Image,
+  View,
+  Text,
+  StyleSheet,
+  ViewStyle,
+  TextStyle,
+  ImageStyle,
+} from "react-native";
+interface Styles {
+  container: ViewStyle;
+  title: TextStyle;
+  categoryImg: ImageStyle;
+}
+interface ItemProps {
+  title: string;
+  image: HTMLImageElement;
+}
+const CategoryList = ({ title, image }: ItemProps) => {
   return (
-    <View>
-      <Text>Hello native</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <Image source={image} style={styles.categoryImg} />
     </View>
   );
 };
 export default CategoryList;
+
+const styles = StyleSheet.create<Styles>({
+  container: {
+    alignItems: "center",
+    backgroundColor: "#fff",
+    padding: 20,
+    // borderRadius: 4,
+    // borderWidth: 1,
+    // borderStyle: "solid",
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    shadowOffset: { width: 0, height: 0 },
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    fontWeight: "700",
+  },
+  categoryImg: {
+    width: 64,
+    height: 64,
+  },
+});
